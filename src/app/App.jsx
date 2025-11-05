ed57e4cbbb1db086d6d3b34d29afbc4f3f2119c7
jaey <jaeyyi98@gmail.com>
2025-11-05 19:10:32 +0900

    feat: 보일러플레이트 코드 추가

---
+++ src/app/App.jsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import { providers } from "./providers";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/app/providers.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const providers = () => {
+  return <div>providers</div>;
+};
