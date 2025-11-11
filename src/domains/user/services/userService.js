0655da3b7e93dc5d5ea807fd9fc1b787ef58a830
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2025-11-11 18:07:20 +0900

    chore: userService.js 파일 대소문자 변경

---
+++ src/domains/user/services/userService.js
@@ -0,0 +1,27 @@
+import { db } from '@/shared/lib/firebase/firestore';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+
+export const createUserProfile = async ({ id, email, name, avatarUrl = '' }) => {
+  const userProfile = {
+    id,
+    email,
+    name,
+    roles: ['USER'],
+    cart: [],
+    enrolledCourseIds: [],
+    createdCourses: [],
+    avatarUrl,
+    createdAt: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+  };
+
+  await setDoc(doc(db, 'users', id), userProfile);
+
+  return userProfile;
+};
+
+// 수정 예정
+export const getUserProfile = async (uid) => {
+  const snap = await getDoc(doc(db, 'users', uid));
+  return snap.exists() ? snap.data() : null;
+};
