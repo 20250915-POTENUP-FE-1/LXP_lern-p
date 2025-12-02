6e87048b5939145020048c7316d0cdcbd8bb98b0
jaey <jaeyyi98@gmail.com>
2025-12-02 17:50:31 +0900

    refactor: Vite에서 Next.js로 프로젝트 마이그레이션

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
