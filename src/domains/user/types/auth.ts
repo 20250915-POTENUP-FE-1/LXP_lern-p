132f861d65753b22a13841af3ed5680941e5b389
whozbebop <whozbebop@gmail.com>
2025-12-05 14:39:56 +0900

    feat: auth.ts, enrolled.ts, user.ts 추가

---
+++ src/domains/user/types/auth.ts
@@ -0,0 +1,21 @@
+export type SignUpForm = {
+  name: string
+  email: string
+  password: string
+  passwordConfirm: string
+}
+export type LoginForm = {
+  email: string
+  password: string
+}
+/** 회원가입 요청에 사용할 Request 타입 */
+export type SignUpRequest = {
+  email: string
+  password: string
+  displayName?: string
+}
+/** 로그인 요청에 사용할 Request 타입 */
+export type LoginRequest = {
+  email: string
+  password: string
+}
