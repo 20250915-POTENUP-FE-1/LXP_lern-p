e69147f9738c091dc5213c5535d77d7c84585cec
jaey <jaeyyi98@gmail.com>
2025-12-05 19:37:43 +0900

    fix: 마이페이지 header 중복 오류 해결

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -4,17 +4,13 @@ import { AppShell } from '@/shared/ui/AppShell';
 
 export default function MyPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AppShell>
-      <section className={`${layout['mypage']} container`} aria-label="마이페이지">
-        <div className={layout['mypage__layout']}>
-          <aside className={layout['mypage__sidebar']}>
-            <MyPageSidebar />
-          </aside>
-          <div className={layout['mypage__content']}>
-            {children}
-          </div>
-        </div>
-      </section>
-    </AppShell>
+    <section className={`${layout['mypage']} container`} aria-label="마이페이지">
+      <div className={layout['mypage__layout']}>
+        <aside className={layout['mypage__sidebar']}>
+          <MyPageSidebar />
+        </aside>
+        <div className={layout['mypage__content']}>{children}</div>
+      </div>
+    </section>
   );
 }
