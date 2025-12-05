c549bb2e5677409b044a28dd27c8c7f4a5a47caa
jaey <jaeyyi98@gmail.com>
2025-12-05 15:31:37 +0900

    로그인 재생버튼 원복

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'images.pexels.com',
+  //     },
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'firebasestorage.googleapis.com',
+  //     },
+  //   ],
+  // },
 };
 
 export default nextConfig;
