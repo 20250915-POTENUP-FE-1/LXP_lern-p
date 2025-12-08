9d6ae42dc0edc84c2c530815eadd45d05550ec56
whozbebop <whozbebop@gmail.com>
2025-12-08 22:52:34 +0900

    chore: 주석 삭제

--- src/app/courses/[id]/learn/page.tsx
+++ src/app/courses/[id]/learn/page.tsx
@@ -1,7 +1,6 @@
 import CourseLearnClient from '@/domains/course/pages/CourseLearnClientPage';
 
 export default function CourseLearnPage() {
-  // Server component: render client component for interactive UI
   return (
     <CourseLearnClient />
   );
