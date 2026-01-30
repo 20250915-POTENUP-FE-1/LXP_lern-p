9307ccc5e24c495f5dd4cc4cc7b963532c6ca2ed
whozbebop <whozbebop@gmail.com>
2026-01-30 16:56:38 +0900

    chore: mock 파일 원복

--- src/mocks/enrollmentList.mock.ts
+++ src/mocks/enrollmentList.mock.ts
@@ -1,19 +1,28 @@
-import type { EnrollmentListResponse } from '@/domains/user/types/enrollment';
 import type { GetEnrollmentResponse } from '@/domains/course/types/course';
+import type { EnrollmentListResponse } from '@/domains/user/types/enrollment';
 
 export const MOCK_ENROLLMENT_LIST: EnrollmentListResponse = {
   content: [
     {
       enrollmentId: '5002',
       courseId: '2002',
-      courseName: '스프링 부트 완벽 가이드',
+      courseName: '스프링 부트 완벽 가이드2',
       status: 'ENROLLED',
       progressRate: 45,
       expiredAt: '2026-01-01T09:00:00',
       categories: ['백엔드', 'Spring'],
     },
+    {
+      enrollmentId: '5004',
+      courseId: '2004',
+      courseName: '데이터베이스 기초',
+      status: 'ENROLLED',
+      progressRate: 20,
+      expiredAt: '2026-03-10T09:00:00',
+      categories: ['데이터베이스'],
+    },
   ],
-  totalElements: 1,
+  totalElements: 4,
   totalPages: 1,
   pageNumber: 0,
   pageSize: 30,
