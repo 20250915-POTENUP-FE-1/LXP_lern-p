119210a6aad873bfdba8e4fbd401ad869ccb1b55
whozbebop <whozbebop@gmail.com>
2026-01-30 15:49:33 +0900

    chore: msw 관련 소스 복구

---
+++ src/app/_providers/msw-provider.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { Suspense, use } from 'react';
+import { handlers } from '@/mocks/handlers';
+
+declare global {
+  interface ImportMeta {
+    hot?: { dispose(cb: () => void): void };
+  }
+}
+const mockingEnabledPromise =
+  typeof window !== 'undefined'
+    ? import('@/mocks/browser').then(async ({ default: worker }) => {
+        if (process.env.NODE_ENV === 'production') {
+          return;
+        }
+        await worker.start({
+          onUnhandledRequest(request, print) {
+            if (request.url.includes('_next')) return;
+            print.warning();
+          },
+        });
+
+        worker.use(...handlers);
+
+        import.meta.hot?.dispose(() => {
+          worker.stop();
+        });
+
+        console.log('[MSW] active handlers:', worker.listHandlers());
+      })
+    : Promise.resolve();
+
+export const MSWProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  );
+};
+
+export const MSWProvider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  use(mockingEnabledPromise);
+  return children;
+};
