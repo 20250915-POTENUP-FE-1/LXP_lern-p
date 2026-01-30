518766c9cf321e167d082f20ee71f8533fd9b0d1
whozbebop <whozbebop@gmail.com>
2026-01-30 16:40:51 +0900

    chore: 불필요한 파일 삭제

--- src/mocks/index.ts
+++
@@ -1,8 +0,0 @@
-export async function initMSW() {
-  if (typeof window === 'undefined') return;
-
-  const { worker } = await import('./browser');
-  await worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
