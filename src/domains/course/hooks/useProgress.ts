a6af9dd980362ed42016443ef01d86ae8d98c979
whozbebop <45797151+whozbebop@users.noreply.github.com>
2026-02-11 19:01:10 +0900

    Apply suggestion from @kimsuyeon0916
    Co-authored-by: Suyeon Kim <kimsuyeon_0916@naver.com>

--- src/domains/course/hooks/useProgress.ts
+++ src/domains/course/hooks/useProgress.ts
@@ -15,13 +15,7 @@ import { getLearnProgress, updateLearnProgress } from '@/domains/course/services
 import { MOCK_LEARN_PROGRESS } from '@/mocks/learn.mock';
 
 export function useProgress(courseId: string) {
-  const [progressData, setProgressData] = useState<GetProgressResponse>({
-    enrollmentId: 0,
-    overallProgressRate: 0,
-    lastWatchedResourceId: 0,
-    lastWatchedAt: '',
-    resourceProgresses: [],
-  });
+const [progressData, setProgressData] = useState<GetProgressResponse | null>(null);
   const [lastSavedResourceId, setLastSavedResourceId] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
