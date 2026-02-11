01bdf7ad16904ca18e257cce10a4ac7eb7428aaf
kimsuyeon_0916 <kimsuyeon_0916@naver.com>
2026-02-11 18:29:05 +0900

    feat: 학습 이력 상세 조회 응답 타입 수정
    - lectureProgresses -> resourceProgresses

--- src/domains/course/types/progress.ts
+++ src/domains/course/types/progress.ts
@@ -13,7 +13,7 @@ export type GetProgressResponse = {
   overallProgressRate: number; // 해당 수강 내역의 전체 진도율
   lastWatchedResourceId: number; // 마지막으로 재생했던 Video ID
   lastWatchedAt: string; // 마지막 학습 활동 시간
-  lectureProgresses: LectureProgressResponse[]; // 개별 강의별 진도 상세 목록
+  resourceProgresses: LectureProgressResponse[]; // 개별 강의별 진도 상세 목록
 };
 
 export type UpdateProgressRequest = {
